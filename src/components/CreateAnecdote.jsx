7d96b8666b98cbe69b754f3eff145949c09a82f9
hectorgarciatw <dreamallica@gmail.com>
2024-09-05 11:04:05 -0300

    Implementation exercises of part 7 (Exercises 7.4-7.6)

--- src/components/CreateAnecdote.jsx
+++ src/components/CreateAnecdote.jsx
@@ -1,42 +1,54 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useField } from "../hooks";
 
 const CreateAnecdote = (props) => {
-    const [content, setContent] = useState("");
-    const [author, setAuthor] = useState("");
-    const [info, setInfo] = useState("");
+    const content = useField("text");
+    const author = useField("text");
+    const info = useField("text");
 
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         props.addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0,
         });
 
         navigate("/");
     };
 
+    // Clean the form inputs using the reset function from useField
+    const cleanInputs = (e) => {
+        e.preventDefault();
+        content.reset();
+        author.reset();
+        info.reset();
+        console.log("Cleaned the inputs");
+    };
+
     return (
         <div>
-            <h2>create a new anecdote</h2>
+            <h2>Create a new anecdote</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                    content
-                    <input name="content" value={content} onChange={(e) => setContent(e.target.value)} />
+                    Content
+                    <input type={content.type} value={content.value} onChange={content.onChange} />
                 </div>
                 <div>
-                    author
-                    <input name="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+                    Author
+                    <input type={author.type} value={author.value} onChange={author.onChange} />
                 </div>
                 <div>
-                    url for more info
-                    <input name="info" value={info} onChange={(e) => setInfo(e.target.value)} />
+                    URL for more info
+                    <input type={info.type} value={info.value} onChange={info.onChange} />
                 </div>
-                <button>create</button>
+                <button type="submit">Create</button>
+                <button type="button" onClick={cleanInputs} style={{ marginLeft: "5px" }}>
+                    Reset
+                </button>
             </form>
         </div>
     );
